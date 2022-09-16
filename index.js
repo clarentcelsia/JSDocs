cd5cf2ffa6ec8f53aefa7887d6a4940784f1c664
Clarenti Celsia <xozhengxiao@gmail.com>
2022-09-16 10:12:52 +0700

    [Feature] Bootstrap

--- index.js
+++ index.js
@@ -3,3 +3,13 @@
 // Print text
 console.log("Hello")
 console.log(1.0)
+
+function enter(){
+    let valids = prompt("Are you sure?");
+    if (valids != null){
+        alert('ans: ' + valids);
+    }
+}
+
+const btn = document.getElementById("btn_enter")
+btn.addEventListener("click", enter)
